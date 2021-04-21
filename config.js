66adec12921f937e8b26ca0977286e0431ae03f5
Steve Ulrich <steveulrich@gmail.com>
2021-04-21 15:42:19 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.GoogleSamples = window.GoogleSamples || {};
 window.GoogleSamples.Config = window.GoogleSamples.Config || {
-  gcmAPIKey: 'AAAA0vwRQZo:APA91bGr5RSaD6u-UgnmhdAAqFIpXCFfV2Ox1-M9yullY40D-bPM-qIHiqjHvUXTxc2_RdUdUC8hIiDtQfUMGrCkvI2eIJaXz7LNPnYfHu3g5yCWz0v2_wIXo2f7zoizaBlNRDQO3lTG'
+  gcmAPIKey: 'BGYBH4UgAAy-6KzmrVmnd21etK4MSjo0p5c3C_vPmLk6Rpl8tgz_N-N_Yb8_mQ5NxW9DQaDewXYyEGwB3hsgVXU'
 };
